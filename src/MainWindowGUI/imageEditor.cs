5dd910f6fbefbcdb6c3b49884fdc36df55cb0453
Alexander Stewart <astew2693@gmail.com>
2025-04-19 13:06:25 -0400

    Update imageEditor.cs
    optimized the image editor crop

--- src/MainWindowGUI/imageEditor.cs
+++ src/MainWindowGUI/imageEditor.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace MainWindowGUI
 {
     public partial class imageEditor : Form
     {
+        private Image originalImage = Image.FromFile(Program.app.inputPath);
         public imageEditor()
         {
             InitializeComponent();
@@ -26,7 +29,7 @@ namespace MainWindowGUI
         {
             try
             {
-                Bitmap original = new Bitmap(Program.app.inputPath); //gets the original image
+                Bitmap original = new Bitmap(originalImage); //gets the original image
 
                 Rectangle cropRect = new Rectangle(0 + int.Parse(leftCropValue.Text), int.Parse(topCropValue.Text), original.Width - int.Parse(leftCropValue.Text) - int.Parse(rightCropValue.Text), original.Height - int.Parse(topCropValue.Text) - int.Parse(bottomCropValue.Text)); //creates a rectangle the size and location of the newly cropped image taking into account all the other cropping values
                 Bitmap cropped = original.Clone(cropRect, original.PixelFormat); //creates the newly cropped image
@@ -52,7 +55,7 @@ namespace MainWindowGUI
             try
             {
                 int cropSize = int.Parse(leftCropValue.Text); //gets the crop size of the image from the text field
-                Bitmap original = new Bitmap(Program.app.inputPath); //gets the original image
+                Bitmap original = new Bitmap(originalImage); //gets the original image
 
                 Rectangle cropRect = new Rectangle(0 + int.Parse(leftCropValue.Text), int.Parse(topCropValue.Text), original.Width - int.Parse(leftCropValue.Text) - int.Parse(rightCropValue.Text), original.Height - int.Parse(topCropValue.Text) - int.Parse(bottomCropValue.Text)); //creates a rectangle the size and location of the newly cropped image taking into account all the other cropping values
                 Bitmap cropped = original.Clone(cropRect, original.PixelFormat); //creates the newly cropped image
@@ -67,7 +70,7 @@ namespace MainWindowGUI
             try
             {
                 int cropSize = int.Parse(bottomCropValue.Text); //gets the crop size of the image from the text field
-                Bitmap original = new Bitmap(Program.app.inputPath); //gets the original image
+                Bitmap original = new Bitmap(originalImage); //gets the original image
 
                 Rectangle cropRect = new Rectangle(0 + int.Parse(leftCropValue.Text), int.Parse(topCropValue.Text), original.Width - int.Parse(leftCropValue.Text) - int.Parse(rightCropValue.Text), original.Height - int.Parse(topCropValue.Text) - int.Parse(bottomCropValue.Text)); //creates a rectangle the size and location of the newly cropped image taking into account all the other cropping values
                 Bitmap cropped = original.Clone(cropRect, original.PixelFormat); //creates the newly cropped image
@@ -82,7 +85,7 @@ namespace MainWindowGUI
             try
             {
                 int cropSize = int.Parse(rightCropValue.Text); //gets the crop size of the image from the text field
-                Bitmap original = new Bitmap(Program.app.inputPath); //gets the original image
+                Bitmap original = new Bitmap(originalImage); //gets the original image
 
                 Rectangle cropRect = new Rectangle(0 + int.Parse(leftCropValue.Text), int.Parse(topCropValue.Text), original.Width - int.Parse(leftCropValue.Text) - int.Parse(rightCropValue.Text), original.Height - int.Parse(topCropValue.Text) - int.Parse(bottomCropValue.Text)); //creates a rectangle the size and location of the newly cropped image taking into account all the other cropping values
                 Bitmap cropped = original.Clone(cropRect, original.PixelFormat); //creates the newly cropped image
