7ecf6bee34369eb574230d115e69a58bffce1b75
Sarvesh <78450215+Sarvesh-Mk@users.noreply.github.com>
2025-05-28 13:22:17 -0400

    fixed batch file

--- run.bat
+++ run.bat
@@ -3,6 +3,19 @@
 if not exist temp (
     mkdir temp
 )
+
+if not exist .venv (
+    echo Creating virtual environment...
+    python -m venv .venv
+    call .venv\Scripts\activate.bat
+    echo Installing dependencies...
+    pip install --upgrade pip >nul 2>&1
+    pip install pygame
+    pip install pygame-ce pygame_gui
+)
+
+call .venv\Scripts\activate.bat
+
 call .venv\Scripts\activate.bat
 python -m src.MainWindowGui.MainWindowGui
 
